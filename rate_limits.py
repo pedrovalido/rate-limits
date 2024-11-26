06efa3098bda5b39c256a449fd80aab178bc1171
Ricardo Airton Silva <ricardoairtonsilva@hotmail.com>
2024-11-26 16:48:43 +0000

    feat: check if new limits required, cleanup unused vars

--- rate_limits.py
+++ rate_limits.py
@@ -7,8 +7,6 @@ LISK_RPC_URL = "https://rpc.api.lisk.com"
 FRAXTAL_RPC_URL = "https://rpc.frax.com"
 web3 = Web3(Web3.HTTPProvider(OPTIMISM_RPC_URL))
 
-rpc_urls = ["https://mainnet.mode.network", "https://rpc.api.lisk.com", "https://rpc.frax.com"]
-
 ROOT_POOL_FACTORY_ADDRESS = "0x31832f2a97Fd20664D76Cc421207669b55CE4BC0"
 CL_ROOT_POOL_FACTORY_ADDRESS = "0x04625B046C69577EfC40e6c0Bb83CDBAfab5a55F"
 VOTER_ADDRESS = "0x41C914ee0c7E1A5edCD0295623e6dC557B5aBf3C"
@@ -33,6 +31,7 @@ minter_abi = '[{"inputs":[{"internalType":"address","name":"_voter","type":"addr
 
 root_pool_abi = '[{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"AlreadyInitialized","type":"error"},{"inputs":[],"name":"chainid","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"factory","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_chainid","type":"uint256"},{"internalType":"address","name":"_token0","type":"address"},{"internalType":"address","name":"_token1","type":"address"},{"internalType":"bool","name":"_stable","type":"bool"}],"name":"initialize","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"stable","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"token0","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"token1","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"}]'
 
+# Data Struct to store Pool information
 class PoolData:
     def __init__(self, address: str):
         self.address = address
@@ -44,6 +43,7 @@ class PoolData:
     def __repr__(self):
         return json.dumps(self.to_dict(), indent=4)
 
+# Data Struct to store Chain information
 class ChainData:
     def __init__(self, name: str, rpc_url: str):
         self.pools = []
@@ -52,9 +52,20 @@ class ChainData:
         self.total_voting_weight = 0
         self.existing_buffer_cap = 0
         self.existing_rate_limit = 0
+        self.expected_emissions = 0
 
     def __repr__(self):
-        return f"Name: {self.name}\nTotal Voting Weight: {self.total_voting_weight}\nExisting Buffer Cap: {self.existing_buffer_cap}\nExisting Rate Limit: {self.existing_rate_limit}\nPools={self.pools}\n"
+        return f"Name: {self.name}\nTotal Voting Weight: {self.total_voting_weight}\nExpected Emissions: {self.expected_emissions}\nExisting Buffer Cap: {self.existing_buffer_cap}\nExisting Rate Limit: {self.existing_rate_limit}\nPools={self.pools}\n"
+
+# Data Struct to store new limits for each Chain
+class NewLimitData:
+    def __init__(self, name: str, new_buffer_cap: int, new_rate_limit: int):
+        self.name = name
+        self.new_buffer_cap = new_buffer_cap
+        self.new_rate_limit = new_rate_limit
+
+    def __repr__(self):
+        return f"Name: {self.name}\nNew Buffer Cap: {self.new_buffer_cap:.0f}\nNew Rate Limit: {self.new_rate_limit:.0f}"
 
 chains: dict[int, ChainData] = {
     34443: ChainData("Mode", MODE_RPC_URL),
@@ -69,13 +80,6 @@ voting_escrow = web3.eth.contract(address=VOTING_ESCROW, abi=voting_escrow_abi)
 minter = web3.eth.contract(address=MINTER, abi=minter_abi)
 
 root_pools = []
-root_pools_by_chain = []
-weights_by_chain = []
-weights_percentage_by_chain = []
-weights_percentage_by_chain_buffered = []
-total_voting_superchain = 0
-existing_buffer_caps_by_chain = []
-existing_rate_limits_by_chain = []
 
 def print_chain_info():
     print("-" * 40)
@@ -84,7 +88,7 @@ def print_chain_info():
         print(chain_data)
         print("-" * 40)  # Divider for clarity
 
-def fetch_pools(log_info: bool = False):
+def fetch_pools():
     # fetch v2 superchain pools
     root_pools = root_pool_factory.functions.allPools().call();
 
@@ -99,30 +103,23 @@ def fetch_pools(log_info: bool = False):
         root_pool = web3.eth.contract(address=pool, abi=root_pool_abi)
         chainid = root_pool.functions.chainid().call()
         chains[chainid].pools.append(PoolData(pool))
-    if (log_info):
-        print_chain_info()
 
-
-def fetch_voting_weights(log_info = False):
-    global total_voting_superchain
+def fetch_voting_weights():
+    superchain_votes = 0
     for chain_id, chain_data in chains.items():
         for pool in chain_data.pools:
             pool.voting_power = voter.functions.weights(pool.address).call()
             chain_data.total_voting_weight += pool.voting_power
-        total_voting_superchain += chain_data.total_voting_weight
+        superchain_votes += chain_data.total_voting_weight
 
-    if (log_info):
-        print_chain_info()
+    return superchain_votes
 
-def fetch_existing_buffers(log_info = False):
+def fetch_existing_buffers():
     for chain_id, chain_data in chains.items():
         web3_temp = Web3(Web3.HTTPProvider(chain_data.rpc_url))
         xerc20 = web3_temp.eth.contract(address=XERC20, abi=xerc20_abi)
         chain_data.existing_buffer_cap = xerc20.functions.bufferCap(MESSAGE_MODULE).call()
         chain_data.existing_rate_limit = xerc20.functions.rateLimitPerSecond(MESSAGE_MODULE).call()
-
-    if (log_info):
-        print_chain_info()
 
 # Main function
 def main():
@@ -130,7 +127,7 @@ def main():
     fetch_pools()
 
     # get each pool votes and sum it up
-    fetch_voting_weights()
+    total_voting_superchain = fetch_voting_weights()
 
     fetch_existing_buffers()
 
@@ -143,22 +140,49 @@ def main():
     print(f"Total Voting Power: {total_voting_weight}")
     print(f"Total Superchain Votes: {total_voting_superchain}")
 
-    pring(f"Total Voting Superchain: {total_voting_superchain}")
+    new_chain_limits: dict[int, NewLimitData] = {}
 
-    for i in range(chains.length):
-        weight = weights_by_chain[i]
-        chain = chain_names[i]
-        weights_percentage_by_chain[i] = weight/total_voting_supply
-        weekly = minter.weekly()
-        expected_emissions = weekly * weights_percentage_by_chain[i]
-        print(f"Chain {chain} - Voting Weight: {weight}")
-        print(f"Chain {chain} - Voting Weight: {weights_percentage_by_chain[i]*100}%")
-        print(f"Chain {chain} - Expected Emissions: {expected_emissions}")
-        print(f"Chain {chain} - Existing Buffer Cap: {existing_buffer_caps_by_chain[i]}")
-        print(f"Chain {chain} - Expected Rate Limit: {expected_emissions/604800}")
-        print(f"Chain {chain} - Existing Rate Limit: {existing_rate_limits_by_chain[i]}")
+    print("-" * 40)
+    for chain_id, chain_data in chains.items():
+        # calculate expected emissions based on chain emissions
+        weights_percentage = chain_data.total_voting_weight/total_voting_weight
+        chain_data.expected_emissions = weekly * weights_percentage * 1.2 # add 1.2x buffer on top of expected emissions
+        print(f"Chain ID: {chain_id}")
+        print(f"Name: {chain_data.name}")
+        print(f"Number of Pools:: {len(chain_data.pools)}")
+        print(f"Pools: {chain_data.pools}")
+        print(f"Chain Voting Weight: {chain_data.total_voting_weight:.0f}")
+        print(f"Percentage of Total Votes: {weights_percentage*100:.3f}%")
+        print(f"Expected Emissions: {chain_data.expected_emissions:.0f}")
+        print(f"Existing Buffer Cap: {chain_data.existing_buffer_cap:.0f}")
+        print(f"Expected Rate Limit: {chain_data.expected_emissions / 604800:.0f}")
+        print(f"Existing Rate Limit: {chain_data.existing_rate_limit:.0f}")
+        if chain_data.expected_emissions > chain_data.existing_buffer_cap:
+            print("*" * 40)
+            print(f"WARNING: Buffer cap should be updated at least to: {chain_data.expected_emissions:.0f}")
+            print(f"WARNING: Rate limit should be updated at least to: {chain_data.expected_emissions / 604800:.0f}")
+            new_chain_limits[chain_id] = NewLimitData(chain_data.name, chain_data.expected_emissions, chain_data.expected_emissions / 604800)
+        print("-" * 40 + "\n")  # Divider for readability
 
+    return new_chain_limits
 
 if __name__ == "__main__":
-    result = main()
-    print("Final Result:", result)
+    new_chain_limits = main()
+
+    if not new_chain_limits:
+        print("No new limits required.")
+        print("-" * 40)
+        for chain_id, chain_data in chains.items():
+            print(f"Chain ID: {chain_id}")
+            print(f"Name: {chain_data.name}")
+            print(f"Chain Voting Weight: {chain_data.total_voting_weight / 1e18:.0f}")
+            print(f"Expected Emissions: {chain_data.expected_emissions / 1e18:.0f}")
+            print(f"Existing Buffer Cap: {chain_data.existing_buffer_cap / 1e18:.0f}")
+            print("-" * 40 + "\n")
+    else:
+        print("WARNING: New Chain Limits required")
+        print("-" * 40)
+        for chain_id, chain_limits in new_chain_limits.items():
+            print(f"Chain ID: {chain_id}")
+            print(chain_limits)
+            print("-" * 40)
