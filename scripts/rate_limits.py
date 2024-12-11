df9c0fbdeb476ea59c4d1ce5eb9c5d82f5041a7a
Ricardo Airton Silva <ricardoairtonsilva@hotmail.com>
2024-12-11 18:17:15 +0000

    chore: update comments

--- scripts/rate_limits.py
+++ scripts/rate_limits.py
@@ -5,6 +5,7 @@ from utils.constants import *
 
 # Setup Optimism RPC
 web3 = Web3(Web3.HTTPProvider(OPTIMISM_RPC_URL))
+
 # Setup Superchain contracts
 cl_root_pool_factory = web3.eth.contract(address=CL_ROOT_POOL_FACTORY_ADDRESS, abi=cl_root_pool_factory_abi)
 root_pool_factory = web3.eth.contract(address=ROOT_POOL_FACTORY_ADDRESS, abi=root_pool_factory_abi)
@@ -42,16 +43,16 @@ def print_summary(chains: dict[int, ChainData]):
         print("-" * 40 + "\n")
 
 def fetch_pools():
-    # fetch v2 superchain pools
+    # Fetch V2 superchain pools
     root_pools = root_pool_factory.functions.allPools().call();
 
-    # fetch cl superchain pools
+    # Fetch CL superchain pools
     cl_root_pools = cl_root_pool_factory.functions.allPools().call();
 
-    # concatenate pool lists
+    # Concatenate pool lists
     root_pools.extend(cl_root_pools)
 
-    # organize by chainid
+    # Organize by chainid
     for pool in root_pools:
         root_pool = web3.eth.contract(address=pool, abi=root_pool_abi)
         chainid = root_pool.functions.chainid().call()
@@ -79,21 +80,22 @@ def fetch_existing_buffers():
         chain_data.existing_midpoint = limit_data[-1]
 
 def check_new_limits(chain_name, expected_emissions, current_limit, adjusted_buffer_cap, adjusted_rps):
-    # recalculate replenished buffer with adjusted limits
+    # Recalculate replenished buffer with adjusted limits
     timestamp = web3.eth.get_block('latest').timestamp
     epoch_next = timestamp - (timestamp % WEEK) + WEEK
-    replenish_ts = epoch_next - (60 * 10) # new limits should replenish 10 minutes before epoch flip
-    # if past last 10 minutes of epoch, limits should replenish until epoch flip instead
+    # New limits should replenish 10 minutes before epoch flip
+    replenish_ts = epoch_next - (60 * 10)
+    # If past last 10 minutes of epoch, limits should replenish until epoch flip instead
     time_to_next_epoch = replenish_ts - timestamp if timestamp < replenish_ts else epoch_next - timestamp
 
     adjusted_midpoint = adjusted_buffer_cap / 2
     expected_buffer = current_limit + (adjusted_rps * time_to_next_epoch)
-    adjusted_replenished_buffer = min(expected_buffer, adjusted_midpoint) # buffer cannot exceed midpoint
-    # if new rps is not enough, need to temporarily set a high rps
+    adjusted_replenished_buffer = min(expected_buffer, adjusted_midpoint) # Buffer cannot exceed midpoint
+    # If new rps is not enough, need to temporarily set a high rps
     print("*" * 40)
     if expected_emissions > adjusted_replenished_buffer:
         buffer_delta = adjusted_midpoint - current_limit
-        temporary_rps = buffer_delta / time_to_next_epoch * RPS_MARGIN # new buffer delta should replenish until epoch flip
+        temporary_rps = buffer_delta / time_to_next_epoch * RPS_MARGIN # New buffer delta should replenish until epoch flip
 
         print(f"WARNING: Buffer cap should be updated at least to: {adjusted_buffer_cap:.0f}")
         print(f"WARNING: Rate Limit should be TEMPORARILY updated to: {temporary_rps:.0f}")
@@ -115,37 +117,38 @@ def check_op_limits(sum_expected_emissions, sum_buffer_delta):
     limit_data = xerc20.functions.rateLimits(MESSAGE_MODULE).call()
     midpoint = limit_data[-1]
 
-    # update root limits
+    # Update root limits
     root_chain.expected_emissions = sum_expected_emissions
     root_chain.existing_buffer_cap = buffer_cap
     root_chain.existing_midpoint = midpoint
     chains[10] = root_chain
 
-    # if buffer not replenished, calculate replenished buffer
+    # If buffer not replenished, calculate replenished buffer
     if current_limit < midpoint:
         timestamp = web3.eth.get_block('latest').timestamp
         epoch_next = timestamp - (timestamp % WEEK) + WEEK
-        replenish_ts = epoch_next - (60 * 10) # new limits should replenish 10 minutes before epoch flip
-        # if past last 10 minutes of epoch, limits should replenish until epoch flip instead
+        # New limits should replenish 10 minutes before epoch flip
+        replenish_ts = epoch_next - (60 * 10)
+        # If past last 10 minutes of epoch, limits should replenish until epoch flip instead
         time_to_next_epoch = replenish_ts - timestamp if timestamp < replenish_ts else epoch_next - timestamp
 
         expected_buffer = current_limit + (rate_limit * time_to_next_epoch)
-        replenished_buffer = min(expected_buffer, midpoint) # buffer cannot exceed midpoint
+        replenished_buffer = min(expected_buffer, midpoint) # Buffer cannot exceed midpoint
     else:
         replenished_buffer = current_limit
 
     if sum_expected_emissions > replenished_buffer or sum_buffer_delta > 0:
-        # increase op buffer cap by sum of buffer deltas
+        # Increase op buffer cap by sum of buffer deltas
         adjusted_buffer_cap = buffer_cap + sum_buffer_delta
         adjusted_midpoint = adjusted_buffer_cap / 2
-        # if not enough, further increase buffer cap
+        # If not enough, further increase buffer cap
         if sum_expected_emissions > adjusted_midpoint:
             adjusted_midpoint = sum_expected_emissions
             adjusted_buffer_cap = adjusted_midpoint * 2
-        # remove buffer margin, apply rps margin instead
+        # Remove buffer margin, apply rps margin instead
         adjusted_rps = ((adjusted_buffer_cap / BUFFER_MARGIN) / WEEK) * RPS_MARGIN
 
-        # log root limits
+        # Log root limits
         print(f"Chain ID: 10")
         print(f"Name: Optimism")
         print(f"Expected Emissions: {sum_expected_emissions:.0f}")
@@ -164,14 +167,14 @@ def main():
     # Fetch pools and organize by chain
     fetch_pools()
 
-    # get each pool votes and sum it up
+    # Get each pool votes and sum it up
     total_voting_superchain = fetch_voting_weights()
 
     fetch_existing_buffers()
 
-    # total voting power at current timestamp
+    # Total voting power at current timestamp
     total_voting_weight = voting_escrow.functions.totalSupply().call()
-    # estimated weekly emissions for next epoch
+    # Estimated weekly emissions for next epoch
     weekly = minter.functions.weekly().call()
 
     # Compare voting weights with total supply
@@ -184,26 +187,26 @@ def main():
     op_expected_emissions = 0
     print("-" * 40)
     for chain_id, chain_data in chains.items():
-        # calculate expected emissions based on chain emissions
+        # Calculate expected emissions based on chain emissions
         weights_percentage = chain_data.total_voting_weight/total_voting_weight
         min_emissions = weekly * weights_percentage
         min_buffer_cap = min_emissions * 2
 
-        # include margin on estimated values
+        # Include margin on estimated values
         chain_data.expected_emissions = min_emissions * BUFFER_MARGIN
         adjusted_buffer_cap = min_buffer_cap * BUFFER_MARGIN
         adjusted_rps = (min_buffer_cap / WEEK) * RPS_MARGIN
         op_buffer_delta += adjusted_buffer_cap - chain_data.existing_buffer_cap if adjusted_buffer_cap > chain_data.existing_buffer_cap else 0
         op_expected_emissions += chain_data.expected_emissions
 
-        # calculate replenished buffer
+        # Calculate replenished buffer
         timestamp = web3.eth.get_block('latest').timestamp
         epoch_next = timestamp - (timestamp % WEEK) + WEEK
         time_to_next_epoch = epoch_next - timestamp
         expected_buffer = chain_data.current_limit + (chain_data.existing_rate_limit * time_to_next_epoch)
-        replenished_buffer = min(expected_buffer, chain_data.existing_midpoint) # buffer cannot exceed midpoint
+        replenished_buffer = min(expected_buffer, chain_data.existing_midpoint) # Buffer cannot exceed midpoint
 
-        # log results
+        # Log results
         print(f"Chain ID: {chain_id}")
         print(f"Name: {chain_data.name}")
         print(f"Number of Pools:: {len(chain_data.pools)}")
